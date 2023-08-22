be2c76c8e0cd8e831c3f8815efd3a23aeba8d2d7
Sachinnegi825 <109611131+Sachinnegi825@users.noreply.github.com>
2023-08-22 12:19:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+// setup date
+const date = (document.getElementById(
+  "date"
+).innerHTML = new Date().getFullYear());
+// setup nav
+const navBtn = document.getElementById("nav-btn");
+const navbar = document.getElementById("navbar");
+const navClose = document.getElementById("nav-close");
+const navlink=document.getElementsByTagName("li");
+// show nav
+navBtn.addEventListener("click", () => {
+  navbar.classList.add("showNav");
+});
+// close nav
+navClose.addEventListener("click", () => {
+  navbar.classList.remove("showNav");
+});
+
+
+for (const link of navlink) {
+  link.addEventListener('click',()=> {
+    navbar.classList.remove("showNav");
+  });
+}
